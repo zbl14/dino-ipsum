41fe78bd8d797a6243a9c92056e4a394b56169b3
zbl14 <ifthereisoneday@gmail.com>
2022-07-03 16:00:02 -0700

    add GiphyService for getting 5 gifs

---
+++ src/services/giphy-service.js
@@ -0,0 +1,13 @@
+export class GiphyService {
+  static async getGif(query) {
+    try {
+      const response = await fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${process.env.GIPHY_API_KEY}&limit=5`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
+  }
+}
