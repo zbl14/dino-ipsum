ff06c02c7b9f9de87e09dbb587f666c16094ea9e
zbl14 <ifthereisoneday@gmail.com>
2022-07-04 09:47:27 -0700

    Fix 401 issue

--- src/services/giphy-service.js
+++ src/services/giphy-service.js
@@ -1,13 +1,13 @@
-export class GiphyService {
+export class GiphyService {  
   static async getGif(query) {
     try {
       const response = await fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${process.env.GIPHY_API_KEY}&limit=5`);
       if (!response.ok) {
         throw Error(response.statusText);
       }
-      return response.json();
-    } catch(error) {
+      return await response.json();
+    } catch (error) {
       return error.message;
-    }
+    } 
   }
 }
